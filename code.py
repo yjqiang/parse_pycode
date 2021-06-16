30d43e1f136a441854f05f2ac63d3dfd970a8fb0
yjqiang <8900942+yjqiang@users.noreply.github.com>
2021-06-16 16:49:16 +0800

    1.0 基本完成

--- code.py
+++ code.py
@@ -1,8 +1,55 @@
+from typing import Tuple
+
+
 import torch
 from torch import nn
-from torch.nn.utils.rnn import pack_padded_sequence as ttt
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import torch.nn.functional as F
 
-F.dfssd()
 
-ttt()
+class MLP(nn.Module):
+    """
+    根据论文 The MLP has a hidden layer with tanh activation and softmax output layer in our experiments.
+    这里损失函数写到 model 里面去
+    """
+    def __init__(self, in_features: int, hidden_features: int, class_num: int):
+        """
+        :param in_features: 输入的数据维数
+        :param hidden_features: 隐藏层的维数
+        :param class_num: 分类数目
+        """
+        super().__init__()
+        self.full_conn1 = nn.Linear(in_features, hidden_features)
+        self.tanh = nn.Tanh()
+        self.full_conn2 = nn.Linear(hidden_features, class_num)
+        self.loss_func = nn.CrossEntropyLoss()
+
+    def get_scores(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        获取分数，为了准确率验证
+        :param x: shape: (batch_size, in_features)
+        :return: scores shape: (batch_size, class_num)
+        """
+        return self.full_conn2(self.tanh(self.full_conn1(x)))
+
+
+class BiLSTM(nn.Module):
+    def __init__(self, input_size: int, hidden_size: int):
+        """
+
+        :param input_size: 输入维度
+        :param hidden_size: h 维数；注意由于 bidirectional 的存在，我们会有 x2 或 /2 操作
+        """
+        super().__init__()
+        self.bi_lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, bidirectional=True, batch_first=True)
+
+    def forward(self, tensor_sentences: torch.Tensor, seq_lengths: torch.Tensor) -> torch.Tensor:
+        """
+        :param tensor_sentences: (batch_size, max_sentence_length, input_size)
+        :param seq_lengths: shape: (batch_size,) ；每句话的实际长度
+        :return: shape: (batch_size, max_sentence_length, 2 * hidden_size)  hidden_size 是说 BiLSTM 初始化输入 hidden_size
+        """
+        packed_sentences = pack_padded_sequence(tensor_sentences, seq_lengths, batch_first=True, enforce_sorted=False)  # 函数内自动进行排序
+        output, _ = self.bi_lstm(packed_sentences)
+        result, _ = pad_packed_sequence(output, batch_first=True)
+        return result
--- main.py
+++ main.py
@@ -1,9 +1,61 @@
 import ast
 
-
-def parse_file(filename: str):
-    with open(filename, encoding='utf-8') as f:
-        return ast.parse(f.read(), filename=filename)
+from de_alias_code import de_alias
 
 
-print(ast.dump(parse_file('code.py'), indent=4))
+def create_parents(root: ast.AST) -> dict[ast.AST, ast.AST]:
+    """
+    原来的树只有父节点指向自节点，现在提供一个反查接口
+    :param root:
+    :return:
+    """
+    parents = {}
+    for cur_node in ast.walk(root):
+        for child in ast.iter_child_nodes(cur_node):
+            parents[child] = cur_node
+    return parents
+
+
+def find_grandparent(node, parents: dict[ast.AST, ast.AST]) -> list[ast.AST]:
+    """
+    找到所有的父节点
+    :param node:
+    :param parents:
+    :return:
+    """
+    parent = node
+    result = [parent]
+    while True:
+        tmp = parents[parent]
+        if not isinstance(tmp, ast.Attribute):
+            return result
+        parent = tmp
+        result.append(parent)
+
+
+def main():
+    path = 'code.py'
+    with open(path, encoding='utf-8') as f:
+        source = f.read()
+
+    source = de_alias(source)
+    list_lines = source.split('\n')
+    print('\n'.join([f'{i:<5}:    {line}' for i, line in enumerate(list_lines, start=1)]))
+
+    tree = ast.parse(source)
+    # print(ast.dump(tree, indent=4))
+    parents = create_parents(tree)
+    for node in ast.walk(tree):  # type: ast.AST
+        if isinstance(node, ast.Name) and node.id == 'torch':
+            list_nodes = find_grandparent(node, parents)
+
+            result = []
+            for cur_node in list_nodes:
+                assert isinstance(cur_node, (ast.Name, ast.Attribute))
+                result.append(cur_node.id if isinstance(cur_node, ast.Name) else cur_node.attr)
+            print(node.lineno, '.'.join(result))
+            # print(ast.dump(node, indent=4))
+
+
+if __name__ == "__main__":
+    main()
